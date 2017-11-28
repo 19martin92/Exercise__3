25664ad287c82e7b26399833db59d270d98959bf
MARTIN\marti_000 <martin@ehrmann-netz.de>
2017-11-28 15:57:51 +0100

    fehlerhaft

--- ProcrustesAligner.h
+++ ProcrustesAligner.h
@@ -26,20 +26,66 @@ public:
 private:
 	Vector3f computeMean(const std::vector<Vector3f>& points) {
 		// TODO: Compute the mean of input points.
+		Vector3f result = Vector3f::Zero();
 
-		return Vector3f::Zero();
+		for (int i = 0; i < points.size(); i++)
+		{
+			result.x() += points[i].x();
+			result.y() += points[i].y();
+			result.z() += points[i].z();
+		}
+		result.x() /= points.size();
+		result.y() /= points.size();
+		result.z() /= points.size();
+
+		return result;
 	}
 
 	Matrix3f estimateRotation(const std::vector<Vector3f>& sourcePoints, const Vector3f& sourceMean, const std::vector<Vector3f>& targetPoints, const Vector3f& targetMean) {
 		// TODO: Estimate the rotation from source to target points, following the Procrustes algorithm. 
 		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
 		
-		return Matrix3f::Identity();
+		// zero-center all points
+		std::vector<Vector3f> sourcePointsZero = sourcePoints;
+		std::vector<Vector3f> targetPointsZero = targetPoints;
+
+		for (int i = 0; i < sourcePoints.size(); i++)
+		{
+			sourcePointsZero[i] -= sourceMean;
+			targetPointsZero[i] -= targetMean;
+		}
+
+		// calculate x and xd
+		Vector3f x = Vector3f::Zero();
+		Vector3f xd = Vector3f::Zero();
+
+		for (int i = 0; i < sourcePointsZero.size(); i++)
+		{
+			x += sourcePointsZero[i];
+			xd += targetPointsZero[i];
+		}
+
+		// create A
+		Matrix3f A = Matrix3f::Identity();
+
+		// A = xT * xd
+		A = x.transpose()*xd;
+
+		// jacobi of matrix A
+		JacobiSVD<Matrix3f> svd(A,ComputeThinU|ComputeThinV);
+
+		// R = U*VT
+		Matrix3f R = svd.matrixU() * svd.matrixV();
+
+		return R;
 	}
 
 	Vector3f computeTranslation(const Vector3f& sourceMean, const Vector3f& targetMean, const Matrix3f& rotation) {
 		// TODO: Compute the translation vector from source to target opints.
-		
-		return Vector3f::Zero();
+		Vector3f result = targetMean;
+
+		result -= rotation * sourceMean;
+
+		return result;
 	}
 };
